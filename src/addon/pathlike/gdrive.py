7dee008a4a2663b11d8a586d95bfef405c129ae1
bluegreenmagick <bluegreenmagick@gmail.com>
2021-08-02 01:25:43 +0900

    add RequestsError and GDrive class

--- src/addon/pathlike/gdrive.py
+++ src/addon/pathlike/gdrive.py
@@ -8,18 +8,63 @@ from .base import FileLike, PathLike
 
 
 API_KEY = os.environ['GOOGLE_DRIVE_API_KEY']
-FIELDS = ["id", "name", "md5Checksum", "mimeType",
-          "fileExtension", "size"]
 
 
-def get_metadata(id: str) -> dict:
-    url = f"https://www.googleapis.com/drive/v3/files/{id}"
-    print(f"getting metadata from {url}")
-    res = requests.get(url, params={
-        "fields": ",".join(FIELDS),
-        "key": API_KEY
-    }).json()
-    return res
+class RequestError(Exception):
+    def __init__(self, code: int, msg: str):
+        super().__init__()
+        self.code = code
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return "RequestError<{self.code}>: {self.msg}"
+
+
+class GDrive():
+    BASE_URL = "https://www.googleapis.com/drive/v3/files"
+    FIELDS_STR = ','.join(["id", "name", "md5Checksum",
+                           "mimeType", "fileExtension", "size"])
+
+    def get_metadata(self, id: str) -> dict:
+        url = f"{self.BASE_URL}/{id}"
+        return self.make_request(url, params={
+            "fields": self.FIELDS_STR,
+            "key": API_KEY
+        }).json()
+
+    def list_paths(self, id: str) -> dict:
+        url = self.BASE_URL
+        return self.make_request(url, params={
+            "q": f"'{id}' in parents",
+            "fields": "files({})".format(self.FIELDS_STR),
+            "key": API_KEY
+        }).json()
+
+    def download_file(self, id: str) -> bytes:
+        url = f"{self.BASE_URL}/{id}"
+        res = self.make_request(url, params={
+            "alt": "media",
+            "key": API_KEY
+        })
+        return res.content
+
+    def make_request(self, url: str, params: dict) -> requests.Response:
+        res = requests.get(url, params)
+        if res.ok:
+            return res
+
+        # Error occured!
+        body = res.json()["error"]
+        code = body["code"]
+        errors = body["errors"]
+        if errors:
+            message = "".join([err["message"] for err in errors])
+        else:
+            message = body["message"]
+        raise RequestError(code, message)
+
+
+gdrive = GDrive()
 
 
 class GDrivePath(PathLike):
@@ -32,7 +77,7 @@ class GDrivePath(PathLike):
             if not id:
                 raise ValueError(
                     "Either data or id should be passed when initializing GDrivePath.")
-            data = get_metadata(id)
+            data = gdrive.get_metadata(id)
         self._data = data
         self._id = data["id"]
         if data["_mimeType"] == "application/vnd.google-apps.folder":
@@ -45,13 +90,7 @@ class GDrivePath(PathLike):
         return self._is_dir
 
     def iterdir(self) -> List["GDrivePath"]:
-        url = "https://www.googleapis.com/drive/v3/files"
-        res = requests.get(url, params={
-            "q": f"'{self._id}' in parents",
-            "fields": "files({})".format(','.join(FIELDS)),
-            "key": API_KEY
-        }).json()
-        print(res)
+        res = gdrive.list_paths(self._id)
         files = res["files"]
         return [GDrivePath(data=file) for file in files]
 
@@ -80,12 +119,7 @@ class GDriveFile(FileLike):
         self._md5 = data["md5"]
 
     def read_bytes(self) -> bytes:
-        url = f"https://www.googleapis.com/drive/v3/files/{self.id}"
-        res = requests.get(url, params={
-            "alt": "media",
-            "key": API_KEY
-        })
-        return res.content
+        return gdrive.download_file(self.id)
 
     @property
     def md5(self) -> str:
