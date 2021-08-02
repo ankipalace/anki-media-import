2fcbd28d48980602a02c9c3ded8b6c0e4b854a87
bluegreenmagick <bluegreenmagick@gmail.com>
2021-08-03 03:03:55 +0900

    add 'fractions' python standard library

--- src/addon/__init__.py
+++ src/addon/__init__.py
@@ -1,8 +1,11 @@
 
 import sys
 from pathlib import Path
+core_dir = Path(__file__).resolve().parent / "core"
+sys.path.append(str(core_dir))
 libs_dir = Path(__file__).resolve().parent / "libs"
 sys.path.append(str(libs_dir))
+
 
 from . import ui
 from . import config
