5dce7e42f409af01f0eec4f10f2bca828f2b915a
bluegreenmagick <bluegreenmagick@gmail.com>
2021-08-04 03:50:34 +0900

    fix import order

--- src/addon/__init__.py
+++ src/addon/__init__.py
@@ -1,8 +1,3 @@
-try:
-    import Crypto
-except:
-    from . import install_libs
-
 
 import sys
 from pathlib import Path
@@ -11,5 +6,9 @@ sys.path.append(str(core_dir))
 libs_dir = Path(__file__).resolve().parent / "libs"
 sys.path.append(str(libs_dir))
 
+try:
+    import Crypto
+except:
+    from . import install_libs
 
 from . import ui
