9841928fbe97f6b27c770b9a0ea2c63d04d7e4b1
bluegreenmagick <bluegreenmagick@gmail.com>
2021-08-04 13:06:20 +0900

    use wheel file instead

--- src/addon/install_libs.py
+++ src/addon/install_libs.py
@@ -1,29 +1,29 @@
 import sys
-import shutil
+import zipfile
 from pathlib import Path
 
 # https://docs.python.org/3/library/platform.html#platform.architecture
 is_64bits = sys.maxsize > 2**32
 
-pycrypto_base = "pycryptodome-3.10.1-cp35-abi3-{}_{}"
+pycrypto_base = "pycryptodome-3.10.1-cp35-abi3-{}{}.whl"
 
 if sys.platform.startswith("linux"):
     if is_64bits:
-        pycrypto_folder = pycrypto_base.format("manylinux1", "x86_64")
+        pycrypto_name = pycrypto_base.format("manylinux1", "_x86_64")
     else:
-        pycrypto_folder = pycrypto_base.format("manylinux1", "i686")
+        pycrypto_name = pycrypto_base.format("manylinux1", "_i686")
 elif sys.platform.startswith("darwin"):
-    pycrypto_folder = pycrypto_base.format("macosx_10_9", "x86_64")
+    pycrypto_name = pycrypto_base.format("macosx_10_9", "_x86_64")
 elif sys.platform.startswith("win32"):
     if is_64bits:
-        pycrypto_folder = pycrypto_base.format("win", "amd64")
+        pycrypto_name = pycrypto_base.format("win", "_amd64")
     else:
-        pycrypto_folder = pycrypto_base.format("win32", "")[:-1]
+        pycrypto_name = pycrypto_base.format("win32", "")
 
-pycrypto_path = Path(__file__).parent / "pycryptodome" / pycrypto_folder
+pycrypto_path = Path(__file__).parent / "pycryptodome" / pycrypto_name
 libs_path = Path(__file__).parent / "libs"
 dist_info_name = "pycryptodome-3.10.1.dist-info"
 
-shutil.copytree(str(pycrypto_path / "Crypto"), str(libs_path / "Crypto"))
-shutil.copytree(str(pycrypto_path / dist_info_name),
-                str(libs_path / dist_info_name))
+with zipfile.ZipFile(pycrypto_path) as zfile:
+    zfile.extractall(libs_path)
+print("Successfully installed pycryptodome for Media Import")
