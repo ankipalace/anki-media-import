05b49de3e5c78b0b4a9ac3fa5c1cdd8a530d7682
Yoonchae Lee <bluegreenmagick@gmail.com>
2022-07-27 12:02:50 +0900

    raise import dialog

--- src/media_import/ui.py
+++ src/media_import/ui.py
@@ -2,6 +2,7 @@ from .dialog import ImportDialog
 
 import_dialog: ImportDialog = None
 
+
 def open_import_dialog() -> None:
 
     global import_dialog
@@ -10,4 +11,4 @@ def open_import_dialog() -> None:
     if not import_dialog.isVisible():
         import_dialog.show()
     import_dialog.activateWindow()
-
+    import_dialog.raise_()
