f667e2ab13c75acc207c3a2bffab59f7e2c3f67a
BlueGreenMagick <bluegreenmagick@gmail.com>
2022-08-01 18:04:07 +0900

    fix anking menu for anki50 (#7)
    * fix anking menu for anki50
    
    * Remove unneccssary import, sort imports
    
    Co-authored-by: RisingOrange <jakub.fidler@protonmail.com>

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,17 @@
-from aqt import gui_hooks
+from typing import Callable
 
-from .anking import setupMenu
+from aqt import gui_hooks, mw
+from aqt.qt import QAction
+
+from .anking import get_anking_menu
 from .media_import import open_import_dialog
+
+
+def setupMenu(handler: Callable[[], None]) -> None:
+    menu = get_anking_menu()
+    a = QAction("Import Media", mw)
+    a.triggered.connect(handler)  # type: ignore
+    menu.addAction(a)
 
 
 gui_hooks.main_window_did_init.append(lambda: setupMenu(open_import_dialog))
