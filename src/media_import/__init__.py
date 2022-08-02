9db483ec0e0ea58da74b052ba46bd6f35bdc6b21
BlueGreenMagick <bluegreenmagick@gmail.com>
2022-08-02 18:23:29 +0900

    Move pycrypto to temp dir if uninstall fails (#9)
    * move pycrypto install code into a function
    
    * rename pycrypto to temp dir if uninstall fails
    
    * Change import
    
    * Fix mypy
    
    * Remove comment
    
    * Remove unused string
    
    Co-authored-by: RisingOrange <jakub.fidler@protonmail.com>

--- src/media_import/__init__.py
+++ src/media_import/__init__.py
@@ -1,14 +1,27 @@
-
 import sys
+from typing import Any, List
 from pathlib import Path
+from aqt import gui_hooks, mw
+
+from .libs import install_pycrypto, uninstall_pycrypto
+
 core_dir = Path(__file__).resolve().parent / "core"
 sys.path.append(str(core_dir))
 libs_dir = Path(__file__).resolve().parent / "libs"
 sys.path.append(str(libs_dir))
 
 try:
-    import Crypto.Cipher
+    from Crypto.Cipher import AES  # type: ignore
 except:
-    from . import install_libs
+    install_pycrypto()
 
 from .ui import open_import_dialog, ImportDialog
+
+
+def on_delete_addon(dial: Any, ids: List[str]) -> None:
+    addon_id = mw.addonManager.addonFromModule(__name__)
+    if addon_id in ids:
+        uninstall_pycrypto()
+
+
+gui_hooks.addons_dialog_will_delete_addons.append(on_delete_addon)
---
+++ src/media_import/libs.py
@@ -0,0 +1,52 @@
+import sys
+import zipfile
+import os
+from tempfile import TemporaryDirectory
+from pathlib import Path
+import shutil
+
+# https://docs.python.org/3/library/platform.html#platform.architecture
+IS_64BITS = sys.maxsize > 2**32
+LIBS_DIR = Path(__file__).parent / "libs"
+
+
+def install_pycrypto() -> None:
+    pycrypto_base = "pycryptodome-3.10.1-cp35-abi3-{}{}.whl"
+
+    if sys.platform.startswith("linux"):
+        if IS_64BITS:
+            pycrypto_name = pycrypto_base.format("manylinux1", "_x86_64")
+        else:
+            pycrypto_name = pycrypto_base.format("manylinux1", "_i686")
+    elif sys.platform.startswith("darwin"):
+        pycrypto_name = pycrypto_base.format("macosx_10_9", "_x86_64")
+    elif sys.platform.startswith("win32"):
+        if IS_64BITS:
+            pycrypto_name = pycrypto_base.format("win", "_amd64")
+        else:
+            pycrypto_name = pycrypto_base.format("win32", "")
+
+    pycrypto_path = Path(__file__).parent / "pycryptodome" / pycrypto_name
+
+    with zipfile.ZipFile(pycrypto_path) as zfile:
+        zfile.extractall(LIBS_DIR)
+    print("Successfully installed pycryptodome for Media Import")
+
+
+def uninstall_pycrypto() -> None:
+    """On Windows, deleting crypto dir may throw an error as .pyd file is open.
+    In that case move(rename) the folder into %TEMP%
+    which may be cleaned up later by the os/user.
+    """
+    pycrypto_path = LIBS_DIR / "Crypto"
+    try:
+        shutil.rmtree(pycrypto_path)
+    except PermissionError:
+        shutil.rmtree(pycrypto_path, ignore_errors=True)  # remove as much as possible
+        tmpdir = TemporaryDirectory()
+        dest_path = Path(tmpdir.name) / "Crypto"
+        os.rename(pycrypto_path, dest_path)
+        try:
+            tmpdir.cleanup()
+        except:
+            pass
