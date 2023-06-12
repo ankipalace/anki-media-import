61695a9bc94ea013ddbecaef20cc87467a332784
RisingOrange <jakub.fidler@protonmail.com>
2023-06-12 13:39:58 +0200

    Undo change to __init__ file

--- src/media_import/__init__.py
+++ src/media_import/__init__.py
@@ -1,8 +1,8 @@
 import sys
 from pathlib import Path
 
-# expose functions
-from .ui import ImportDialog, open_import_dialog # noqa: F401
-
 libs_dir = Path(__file__).resolve().parent / "libs"
 sys.path.append(str(libs_dir))
+
+# expose functions
+from .ui import open_import_dialog, ImportDialog
